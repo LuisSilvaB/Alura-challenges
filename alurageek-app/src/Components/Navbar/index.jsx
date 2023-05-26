1dbaaa3d4e17719c7fe492a64b68b71bd38d04c6
LuisSilvaB <luissilvaballadares@hotmail.com>
2023-05-25 19:10:10 -0500

    AluraGeek: Add - Navbar

---
+++ alurageek-app/src/Components/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Icono from '../../Img/Inicio/Vector.svg'
+import Alura from '../../Img/Inicio/Alura.svg'
+import Geek from '../../Img/Inicio/Geek.svg'
+import SearchIcon from '../../Img/Inicio/SeacrhIcon.svg'
+import './Navbar.css'; 
+
+
+const Navbar = () => {
+  return (
+    <nav className='Navbar'>
+      <h1 className='TituloAlura'>
+        <img src = { Icono } alt='Icono Alura Geek'/>
+        <img src = { Alura } alt='Icono Alura'/>
+        <img src = { Geek } alt='Icono Geek'/>
+      </h1>
+      <div className='containerSarchButtons'>  
+        <div className='SearchBar'>
+          <input className='SearchBar__input' type='text' placeholder='¿Qué deseas buscar?'/>
+          <img className='SearchBar__icono' src = { SearchIcon }/>
+        </div>
+        <div className='ContainerButtons'>
+          <button className='ContainerButtons__registro'> Registrarse </button>
+          <button className='ContainerButtons__iniciarSesion'> Iniciar Sesión </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
